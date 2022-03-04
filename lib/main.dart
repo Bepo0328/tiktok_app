fc293de3934f2140dd89f06c7ce8cecef258b2d4
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-04 21:19:10 +0900

    TiktokApp: Authentication Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_app/constants.dart';
 import 'package:tiktok_app/views/screens/auth/login_screen.dart';
 import 'package:tiktok_app/views/screens/auth/sign_up_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCNDNaJrB8r_uPCKeHGFFfHlS6IrYLQ-Jw",
+        authDomain: "tiktok-app-fef3a.firebaseapp.com",
+        projectId: "tiktok-app-fef3a",
+        storageBucket: "tiktok-app-fef3a.appspot.com",
+        messagingSenderId: "1037775070595",
+        appId: "1:1037775070595:web:41f0290b7412bb9e3a02b6",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -11,7 +31,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone App',
       theme: ThemeData.dark().copyWith(
