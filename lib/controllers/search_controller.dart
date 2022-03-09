58fb6e055c691e05b2395b438ccabee94e40a889
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-09 22:26:30 +0900

    TiktokApp: Create Search Screen UI

---
+++ lib/controllers/search_controller.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_app/constants.dart';
+import 'package:tiktok_app/models/user.dart';
+
+class SearchController extends GetxController {
+  final Rx<List<User>> _searchUsers = Rx<List<User>>([]);
+  List<User> get searchUsers => _searchUsers.value;
+
+  searchUser(String typedUser) async {
+    _searchUsers.bindStream(firestore
+        .collection('users')
+        .where('name', isGreaterThanOrEqualTo: typedUser)
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<User> retVal = [];
+      for (var element in query.docs) {
+        retVal.add(User.fromSnap(element));
+      }
+      return retVal;
+    }));
+  }
+}
